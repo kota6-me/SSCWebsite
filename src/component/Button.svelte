8ef89cb68093f1f4470a8a4d2e14b901037583a9
kota6-me <contact@kota6.me>
2024-12-19 01:13:24 +0900

    initial commit

---
+++ src/component/Button.svelte
@@ -0,0 +1,16 @@
+<button on:click={handleClick}>
+  {num}
+</button>
+<button on:click={handleReset}>
+  reset
+</button>
+
+<script>
+  let num = 0;
+  function handleClick() {
+    num += 1;
+  }
+  function handleReset() {
+    num = 0;
+  }
+</script>
