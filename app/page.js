2348b434ea149e588d92dcb423a9a9503b98516a
kota6-me <contact@kota6.me>
2024-12-24 23:00:12 +0900

    top

--- app/page.js
+++ app/page.js
@@ -1,7 +1,27 @@
 export default function Home() {
   return (
     <div>
-      <h1>S²C Official Website</h1>
+      <p>
+        <span class="text-black font-urb font-bold text-5xl tracking-[-0.14em]">
+          S²C
+        </span>
+        <span class="text-black font-urb font-bold text-5xl tracking-tight">
+          {" "}
+          Official Website
+        </span>
+      </p>
+      <Button client:load />
+      <code> let hoge = "fuga"; </code>
+      <div>
+        NEON:
+        <br />
+        ID:{response[0].id}
+        <br />
+        ITEM:{response[0].data_name}
+        <br />
+        PRICE:¥{response[0].price_yen}.
+      </div>
+      <a href="/login/">Login</a>
     </div>
   );
 }
