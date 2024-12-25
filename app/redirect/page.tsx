3e114123ea4bbbaf9efdb8c63eac62740050b6af
kota6-me <contact@kota6.me>
2024-12-26 04:08:41 +0900

    しゅうせ

--- app/redirect/page.tsx
+++ app/redirect/page.tsx
@@ -2,43 +2,15 @@
 import React, { useEffect } from "react";
 import { Suspense } from "react";
 import { useSearchParams } from "next/navigation";
-import {
-  grantDiscordAccessToken,
-  getDiscordUserData,
-} from "../utils/discordLogin";
-import type { typeGrantAccess, typeUserData } from "../utils/discordLogin";
-let grantAccess: typeGrantAccess = {
-  access_token: null,
-  token_type: null,
-  expires_in: null,
-  refresh_token: null,
-  scope: null,
-  isSuccessfull: false,
-  errorDescription: null,
-};
-let userData: typeUserData = {
-  avatar: null,
-  banner: null,
-  email: null,
-  global_name: null,
-  id: null,
-  username: null,
-  isSuccessfull: false,
-  errorDescription: null,
-};
 const searchParams = useSearchParams();
 const code = searchParams.get("code") || "";
 
 export default function Redirect() {
   return (
-    <Suspense
-      fallback={
-        <div>
-          Loading...
-          <br />
-          {code}
-        </div>
-      }
-    ></Suspense>
+    <div>
+      Loading...
+      <br />
+      <p>{code}</p>
+    </div>
   );
 }
