be22bae3c59000e673006cc75a60f9107ef2f870
kota6-me <contact@kota6.me>
2024-12-26 01:00:33 +0900

    一度codeが出力できてるか

--- app/redirect/page.tsx
+++ app/redirect/page.tsx
@@ -29,44 +29,6 @@ let userData: typeUserData = {
 const searchParams = useSearchParams();
 const code = searchParams.get("code") || "";
 
-function CodeWorker() {
-  useEffect(() => {
-    grantDiscordAccessToken(code).then((res) => {
-      grantAccess = res;
-      if (grantAccess.isSuccessfull) {
-        getDiscordUserData(grantAccess.access_token).then((res) => {
-          userData = res;
-          if (userData.isSuccessfull) {
-            console.log("ログイン成功");
-          } else {
-            console.log("ログイン失敗");
-          }
-        });
-      } else {
-        console.log("ログイン失敗");
-      }
-    });
-  }, [code]);
-}
-
-function PrintData() {
-  CodeWorker();
-  return (
-    <ul>
-      <li>ユーザー名：{userData.global_name}</li>
-      <li>ユーザーID：{userData.username}</li>
-      <li>SnowflakeID：{userData.id}</li>
-      <li>Email:{userData.email}</li>
-      <li>
-        Avatar:{`https://cdn.discordapp.com/${userData.id}/${userData.avatar}`}
-      </li>
-      <li>
-        Banner:{`https://cdn.discordapp.com/${userData.id}/${userData.banner}`}
-      </li>
-    </ul>
-  );
-}
-
 export default function Redirect() {
   return (
     <Suspense
@@ -77,8 +39,6 @@ export default function Redirect() {
           {code}
         </div>
       }
-    >
-      <PrintData />
-    </Suspense>
+    ></Suspense>
   );
 }
