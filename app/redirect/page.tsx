4e69fb7e492c73ea499d78c371eb881162d98159
kota6-me <contact@kota6.me>
2024-12-26 13:37:02 +0900

    aaa

--- app/redirect/page.tsx
+++ app/redirect/page.tsx
@@ -1,8 +1,12 @@
+"use client";
+import { useSearchParams } from "next/navigation";
 export default function Redirect() {
+  let searchParams = useSearchParams();
+  let code = searchParams.get("code");
   return (
     <div>
       <h1>Redirect</h1>
-      <p>Code: DummyCode</p>
+      <p>Code: {code}</p>
     </div>
   );
 }
