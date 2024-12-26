692a6cdc73ed5e064ce9f5e8fc8ce206d709c483
kota6-me <contact@kota6.me>
2024-12-26 13:38:53 +0900

    aa

--- app/redirect/page.tsx
+++ app/redirect/page.tsx
@@ -1,12 +1,15 @@
 "use client";
 import { useSearchParams } from "next/navigation";
+import { Suspense } from "react";
 export default function Redirect() {
   let searchParams = useSearchParams();
   let code = searchParams.get("code");
   return (
     <div>
       <h1>Redirect</h1>
-      <p>Code: {code}</p>
+      <Suspense>
+        <p>{code}</p>
+      </Suspense>
     </div>
   );
 }
